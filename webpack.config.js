99d69cec51fcc5d24790e858ce3cc2147ef68675
Jeffrey Rehm <jrehm@student.neumont.edu>
2021-07-27 11:57:45 -0600

    Basic webpack/babel/scss server

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+module.exports = {
+  "mode": "none",
+  "entry": "./src/index.js",
+  "output": {
+    "path": __dirname + '/dist',
+    "filename": "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 80 //TODO use 443 with HTTPS
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        "test": /\.js$/,
+        "exclude": /node_modules/,
+        "use": {
+          "loader": "babel-loader",
+          "options": {
+            "presets": [
+              "@babel/preset-env",
+            ]
+          }
+        }
+      },
+    ]
+  }
+}
