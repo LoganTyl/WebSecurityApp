0319606bf1da66b5740f34a42863a2cadc16b79a
LoganTyl <47304609+LoganTyl@users.noreply.github.com>
2021-07-30 17:06:23 -0600

    Disposed of some config stuff that was causing a crash and implemented a hello world page

--- webpack.config.js
+++ webpack.config.js
@@ -6,180 +6,156 @@ const TerserWebpackPlugin = require("terser-webpack-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
-module.exports = {
-  function(_env, argv){
-      const isProduction = argv.mode === "production";
-      const isDevelopment = !isProduction;
+module.exports = function(_env, argv){
+    const isProduction = argv.mode === "production";
+    const isDevelopment = !isProduction;
 
-      return {
+    return {
         devtool: isDevelopment && "cheap-module-source-map",
         entry: "./src/index.tsx",
         // entry: "./src/index.js",
         output: {
-          path: path.resolve(__dirname, "dist"),
-          filename: "assets/js/[name].[contenthash:8].js",
-          publicPath: "/"
-        }
-      };
-  },
-  "mode": "none",
-  "entry": "./src/index.js",
-  "output": {
-    "path": __dirname + '/dist',
-    "filename": "bundle.js"
-  },
-  devServer: {
-    compress: true,
-    historyApiFallback: true,
-    open: true,
-    overlay: true,
-    contentBase: path.join(__dirname, 'dist'),
-    port: 80 //TODO use 443 with HTTPS
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            path: path.resolve(__dirname, "dist"),
+            filename: "assets/js/[name].[contenthash:8].js",
+            publicPath: "/"
+        },
+        "mode": "none",
+        "entry": "./src/index.js",
+        "output": {
+          "path": __dirname + '/dist',
+          "filename": "bundle.js"
+        },
+        devServer: {
+          compress: true,
+          historyApiFallback: true,
+          open: true,
+          overlay: true,
+          contentBase: path.join(__dirname, 'dist'),
+          port: 80 //TODO use 443 with HTTPS
+        },
+        module: {
+          rules: [
             {
-                loader: "css-loader",
-                options: {
-                    importLoaders: 2
-                }
+              test: /\.s[ac]ss$/i,
+              use: [
+                  isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+                  {
+                      loader: "css-loader",
+                      options: {
+                          importLoaders: 2
+                      }
+                  },
+                  'resolve-url-loader',
+                  {
+                      loader: "sass-loader",
+                      options: {
+                          sourceMap: true
+                      }
+                  }
+              //   "style-loader",
+              //   "css-loader",
+              //   "sass-loader"
+              ]
             },
-            'resolve-url-loader',
             {
-                loader: "sass-loader",
-                options: {
-                    sourceMap: true
+                test: /\.css$/,
+                use: [
+                    isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.module.css$/,
+                use: [
+                    isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            },
+            {
+              'test': /\.(js|jsx|ts|tsx)$/,
+              // "test": /\.jsx?$/,
+              "exclude": /node_modules/,
+              "use": {
+                "loader": "babel-loader",
+                "options": {
+                    cacheDirectory: true,
+                    cacheCompression: false,
+                    envName: isProduction ? "production" : "development",
+                  "presets": [
+                    "@babel/preset-env",
+                  ]
                 }
-            }
-        //   "style-loader",
-        //   "css-loader",
-        //   "sass-loader"
-        ]
-      },
-      {
-          test: /\.css$/,
-          use: [
-              isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
-              'css-loader'
-          ]
-      },
-      {
-          test: /\.module.css$/,
-          use: [
-              isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
-              {
-                  loader: 'css-loader',
+              }
+            },
+            {
+              test: /\.(png|jpg|gif)$/i,
+              use: {
+                  loader: 'url-loader',
                   options: {
-                      modules: true
+                      limit: 8192,
+                      name: "static/media/[name].[hash:8].[ext]"
                   }
               }
-          ]
-      },
-      {
-        'test': /\.(js|jsx|ts|tsx)$/,
-        // "test": /\.jsx?$/,
-        "exclude": /node_modules/,
-        "use": {
-          "loader": "babel-loader",
-          "options": {
-              cacheDirectory: true,
-              cacheCompression: false,
-              envName: isProduction ? "production" : "development",
-            "presets": [
-              "@babel/preset-env",
-            ]
-          }
-        }
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: {
-            loader: 'url-loader',
-            options: {
-                limit: 8192,
-                name: "static/media/[name].[hash:8].[ext]"
-            }
-        }
-      },
-      {
-        test: /\.svg$/,
-        use: ["@svgr/webpack"]
-      },
-      {
-          test: /\.(eot|otf|ttf|woff|woff2)$/,
-          loader: require.resolve('file-loader'),
-          options: {
-              name: "static/edia/[name].[hash:8].[ext]"
-          }
-      }
-    ]
-  },
-  resolve: {
-      extensions: [".js", ".jsx", '.ts', '.tsx']
-  },
-  plugins: [
-      isProduction && new MiniCssExtractPlugin({
-          filename: 'assets/css/[name].[contenthash:8].css',
-          chunkFilename: 'assets/css/[name].[contenthash:8].chunk.css'
-      }),
-      new webpack.DefinePlugin({
-          "process.env.NODE_ENV": JSON.stringify(
-              isProduction ? "production" : "development"
-          )
-      }),
-      new HtmlWebpackPlugin({
-          template: path.resolve(__dirname, "public/index.html"),
-          inject: true
-      }),
-      new ForkTsCheckerWebpackPlugin({
-          async: false
-      })
-  ].filter(Boolean),
-  optimization: {
-      minimize: isProduction,
-      minimizer: [
-          new TerserWebpackPlugin({
-              terserOptions: {
-                  compress: {
-                      comparisons: false
-                  },
-                  mangle: {
-                      safari10: true
-                  },
-                  output: {
-                      comments: false,
-                      ascii_only: true
-                  },
-                  warnings: false
-              }
-          }),
-          new OptimizeCssAssetsPlugin()
-      ],
-      splitChunks: {
-        chunks: 'all',
-        minSize: 0,
-        maxInitialRequests: 20,
-        maxAsyncRequests: 20,
-        cacheGroups: {
-            vendors: {
-                test: /[\\/]node_modules[\\/]/,
-                name(module, chunks, cacheGroupKey) {
-                    const packageName = module.content.match(
-                    /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-                    )[1];
-                    return `${cacheGroupKey}.${packageName.replace("@", "")}`;
-                }
             },
-            common: {
-                minChunks: 2,
-                priority: -10
+            {
+              test: /\.svg$/,
+              use: ["@svgr/webpack"]
+            },
+            {
+                test: /\.(eot|otf|ttf|woff|woff2)$/,
+                loader: require.resolve('file-loader'),
+                options: {
+                    name: "static/edia/[name].[hash:8].[ext]"
+                }
             }
+          ]
+        },
+        resolve: {
+            extensions: [".js", ".jsx", '.ts', '.tsx']
+        },
+        plugins: [
+            isProduction && new MiniCssExtractPlugin({
+                filename: 'assets/css/[name].[contenthash:8].css',
+                chunkFilename: 'assets/css/[name].[contenthash:8].chunk.css'
+            }),
+            new webpack.DefinePlugin({
+                "process.env.NODE_ENV": JSON.stringify(
+                    isProduction ? "production" : "development"
+                )
+            }),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, "src/pages/index.html"),
+                inject: true
+            }),
+            new ForkTsCheckerWebpackPlugin({
+                async: false
+            })
+        ].filter(Boolean),
+        optimization: {
+            minimize: isProduction,
+            minimizer: [
+                new TerserWebpackPlugin({
+                    terserOptions: {
+                        compress: {
+                            comparisons: false
+                        },
+                        mangle: {
+                            safari10: true
+                        },
+                        output: {
+                            comments: false,
+                            ascii_only: true
+                        },
+                        warnings: false
+                    }
+                }),
+                new OptimizeCssAssetsPlugin()
+            ]
         }
-      },
-      runtimeChunk: "single"
-  }
+    }
 }
