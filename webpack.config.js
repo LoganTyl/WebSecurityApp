98a854edc2628a2aea5e64477081df7e7621b94c
Jeffrey Rehm <jrehm@student.neumont.edu>
2021-08-02 10:24:14 -0600

    Updated webpack.config.js to add express
    Didn't fix anything

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path');
+
 module.exports = {
-  "mode": "none",
-  "entry": "./src/index.js",
-  "target": "node",
-  "output": {
-    "path": __dirname + '/dist',
-    "filename": "bundle.js"
+  mode: 'none',
+  entry: './src/index.js',
+  target: 'node',
+  // node: {
+    // process: false,
+    // Buffer: false,
+    // require: false,
+  // },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: 'http://localhost/',
+    filename: 'bundle.js'
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
@@ -16,23 +23,32 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader"
+          'style-loader',
+          'css-loader',
+          'sass-loader'
         ]
       },
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   use: ['source-map-loader']
+      // },
       {
-        "test": /\.js$/,
-        "exclude": /node_modules/,
-        "use": {
-          "loader": "babel-loader",
-          "options": {
-            "presets": [
-              "@babel/preset-env",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
             ]
           }
         }
-      },
+      }
     ]
+  },
+  externals: {
+    express: 'express',
+    // express: require('express'),
   }
 }
